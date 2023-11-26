a6be7fb6635ca889029a34c0593450a8b3149b10
Zeifyra <s.rodriguez63@uniandes.edu.co>
2023-11-25 20:10:40 -0500

    Cambios

--- lab.r
+++ lab.r
@@ -1,5 +1,22 @@
+# Cargamos el Archivo CSV
 
-df = read.table("datat.csv",  header=TRUE, sep=";")
+data <- read.csv("datat.csv", header = TRUE, sep = ";")
 
-ff <- freqCalc(df, keyVars=1:ncol(df), w=34)
-write.table((cbind(x, ff$fk, ff$Fk)), 'output.csv', sep='\t', )
+x <- data # Eliminas la ultima columna
+
+#Cambios en el dataset
+
+x[,7] <- globalRecode(x[ ,7],breaks=c(0,2,3,4),labels=c(1,2,3))
+x[,8] <- globalRecode(x[ ,8],breaks=c(0,2,3,4),labels=c(1,2,3))
+x[,3] <- globalRecode(x[ ,3],breaks=c(15,16,17,18,22),labels=c(16,18,19,21))
+
+localsupx <- kAnon(x, keyVars=c(13,15), k=10)
+
+# Calculas la frecuencia de todas las columnas del mismo
+
+ff <- freqCalc(x, keyVars = names(x), w = 34)
+rk <- indivRisk(ff)$rk
+
+#Escribimos el resultado en un archivo CSV
+
+write.table(cbind(x,ff$fk,ff$Fk,rk), file = "result.csv", sep = ";", row.names = FALSE)
